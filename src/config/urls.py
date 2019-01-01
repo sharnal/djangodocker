2c7fe906c824e1e91bed4a3e27e0c473621c9f1f
Shubham Harnal <shubham.harnal@live.com>
2019-01-01 22:55:20 +0530

    Added sample home page

--- src/config/urls.py
+++ src/config/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from organizer import urls as organizer_urls
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include(organizer_urls)),
 ]
--- src/organizer/views.py
+++ src/organizer/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.http import (
+    require_http_methods
+)
 
+@require_http_methods(["GET", "HEAD"])
+def hello_world(request):
+    return HttpResponse("Hello world!")
 # Create your views here.
